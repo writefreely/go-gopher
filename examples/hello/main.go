7860a987603d3a1e29eaec2d28975258dc588205
James Mills <prologic@shortcircuit.net.au>
2016-09-14 16:23:46 +1000

    Improve hello example with FILE and DIR handlers

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -9,9 +9,16 @@ import (
 func index(w gopher.ResponseWriter, r *gopher.Request) {
 	w.WriteItem(
 		gopher.Item{
-			Type:        gopher.FILE,
+			Type:        gopher.DIRECTORY,
 			Selector:    "/hello",
 			Description: "hello",
+		},
+	)
+	w.WriteItem(
+		gopher.Item{
+			Type:        gopher.FILE,
+			Selector:    "/foo",
+			Description: "foo",
 		},
 	)
 }
@@ -20,8 +27,13 @@ func hello(w gopher.ResponseWriter, r *gopher.Request) {
 	w.WriteInfo("Hello World!")
 }
 
+func foo(w gopher.ResponseWriter, r *gopher.Request) {
+	w.Write([]byte("Foo!"))
+}
+
 func main() {
 	gopher.HandleFunc("/", index)
+	gopher.HandleFunc("/foo", foo)
 	gopher.HandleFunc("/hello", hello)
 	log.Fatal(gopher.ListenAndServe("localhost:70", nil))
 }
